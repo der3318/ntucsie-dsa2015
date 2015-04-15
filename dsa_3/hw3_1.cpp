55ea6b656fc1571444067720f98639cfe5647297
b03902015 <b03902015@linux2.csie.ntu.edu.tw>
2015-04-16 04:03:03 +0800

    hw3

---
+++ dsa_3/hw3_1.cpp
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stack>
+#include <iostream>
+
+#define MAX 1100000
+#define toChar_MAX 200
+#define toInt_MAX 256
+#define BNT 10
+#define NOT 11
+#define MIN 12
+#define PLU 13
+#define MUL 20
+#define DIV 21
+#define MOD 22
+#define ADD 30
+#define SUB 31
+#define SHL	40
+#define SHR 41
+#define	BAN 50
+#define XOR 60
+#define BOR 70
+#define AND 80
+#define OR 90
+#define PAR 100
+
+/*
+
+Sign-Value Table
+					(m)	(p)											(a)	(o)
+Sign		~	!	-	+	*	/	%	+	-	<<	>>	&	^	|	&&	||	(
+Value		-10	-11	-12	-13	-20	-21	-22	-30	-31	-40	-41	-50	-60	-70	-80	-90	-100
+
+*/
+
+char infix[MAX],tmp[MAX], toChar[toChar_MAX][3];
+int postfix[MAX], toInt[toInt_MAX];
+
+void toPostfix()
+{
+	std::stack<int> op_stack;
+	int i = 0, p = 0;
+	while(infix[i] != '\n')
+	{
+		if(infix[i] <= '9' && infix[i] >= '0')
+		{
+			postfix[p] = (int)(infix[i++] - '0');
+			while(infix[i] <= '9' && infix[i] >= '0')	postfix[p] = postfix[p] * 10 + (int)(infix[i++] - '0');
+			p++;	i--;
+		}
+		else if(infix[i] == '(' || infix[i] == '~' || infix[i] == '!' || infix[i] == 'm')
+		{
+			op_stack.push( toInt[ (int)infix[i] ] );
+			printf("Push \"%s\" into the stack\n", toChar[ -op_stack.top() ]);
+		}
+		else if(infix[i] == ')')
+		{
+			while(op_stack.top() != toInt[ (int)'(' ])
+			{
+				postfix[p++] = op_stack.top();
+				printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ] );
+				op_stack.pop();
+			}
+			printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ] );
+			op_stack.pop();
+		}
+		else
+		{
+			int pos = toInt[ (int)infix[i] ] ;
+			if(infix[i] == infix[i + 1])
+			{
+				i++;
+				if(infix[i] == '&')	pos = -AND;
+				else if(infix[i] == '|')	pos = -OR;
+			}
+			while((!op_stack.empty()) && ((-op_stack.top()) / 10) <= ((-pos) / 10))
+			{
+				postfix[p++] = op_stack.top();
+				printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ] );
+				op_stack.pop();
+			}
+			op_stack.push(pos);
+			printf("Push \"%s\" into the stack\n", toChar[ -op_stack.top() ]);
+		}
+		i++;
+	}
+	while(!op_stack.empty())
+	{
+		postfix[p++] = op_stack.top();
+		printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ] );
+		op_stack.pop();
+	}
+	postfix[p] = -1;
+	return;
+}
+
+void toValue()
+{
+	std::stack<int> val_stack;
+	int p = 0, v_1 = 0, v_2 = 0;
+	while(postfix[p] != -1)
+	{
+		if(postfix[p] >= 0)	val_stack.push(postfix[p]);
+		else
+		{
+			v_2 = val_stack.top();
+			val_stack.pop();
+			if(postfix[p] == -BNT)	val_stack.push(~v_2);
+			else if(postfix[p] == -NOT)	val_stack.push(!v_2);
+			else if(postfix[p] == -MIN)	val_stack.push(-v_2);
+			else
+			{
+				v_1 = val_stack.top();
+				val_stack.pop();
+				if(postfix[p] == -ADD)	val_stack.push(v_1 + v_2);
+				else if(postfix[p] == -SUB)	val_stack.push(v_1 - v_2);
+				else if(postfix[p] == -MUL)	val_stack.push(v_1 * v_2);
+				else if(postfix[p] == -DIV)	val_stack.push(v_1 / v_2);
+				else if(postfix[p] == -MOD)	val_stack.push(v_1 % v_2);
+				else if(postfix[p] == -BAN)	val_stack.push(v_1 & v_2);
+				else if(postfix[p] == -XOR)	val_stack.push(v_1 ^ v_2);
+				else if(postfix[p] == -BOR)	val_stack.push(v_1 | v_2);
+				else if(postfix[p] == -SHL)	val_stack.push(v_1 << v_2);
+				else if(postfix[p] == -SHR)	val_stack.push(v_1 >> v_2);
+				else if(postfix[p] == -AND)	val_stack.push(v_1 && v_2);
+				else if(postfix[p] == -OR)	val_stack.push(v_1 || v_2);
+			}
+		}
+		p++;
+	}
+	if(val_stack.size() != 1)	printf("Error Form.\n");
+	else	printf("RESULT: %d\n", val_stack.top());
+	return;
+}
+
+int main()
+{
+	toInt[(int)'+'] = -ADD;	strcpy(toChar[ADD], "+");
+	toInt[(int)'-'] = -SUB;	strcpy(toChar[SUB], "-");
+	toInt[(int)'*'] = -MUL;	strcpy(toChar[MUL], "*");
+	toInt[(int)'/'] = -DIV;	strcpy(toChar[DIV], "/");
+	toInt[(int)'%'] = -MOD;	strcpy(toChar[MOD], "%");
+	toInt[(int)'&'] = -BAN;	strcpy(toChar[BAN], "&");
+	toInt[(int)'^'] = -XOR;	strcpy(toChar[XOR], "^");
+	toInt[(int)'|'] = -BOR;	strcpy(toChar[BOR], "|");
+	toInt[(int)'~'] = -BNT;	strcpy(toChar[BNT], "~");
+	toInt[(int)'!'] = -NOT;	strcpy(toChar[NOT], "!");
+	toInt[(int)'m'] = -MIN;	strcpy(toChar[MIN], "m");
+	toInt[(int)'<'] = -SHL;	strcpy(toChar[SHL], "<<");
+	toInt[(int)'>'] = -SHR;	strcpy(toChar[SHR], ">>");
+	toInt[(int)'a'] = -AND;	strcpy(toChar[AND], "&&");
+	toInt[(int)'o'] = -OR;	strcpy(toChar[OR], "||");
+	toInt[(int)'('] = -PAR;	strcpy(toChar[PAR], "(");
+	while(fgets(tmp, MAX, stdin) != NULL)
+	{
+		if(tmp[1] == '\0')	break;
+		int j = 0;
+		for(int i = 0 ; tmp[i] != '\n' ; i++)
+			if(tmp[i] != ' ')
+			{
+				if(tmp[i] == '+' || tmp[i] == '-')
+					if(j == 0 || ((infix[j - 1] > '9' || infix[j - 1] < '0') && infix[j - 1] != ')'))
+					{
+						if(tmp[i] == '-')	infix[j++] = 'm';
+						continue;
+					}
+				infix[j++] = tmp[i];
+			}
+		infix[j] = '\n';
+		toPostfix();
+		printf("Postfix Expression: ");
+			for(int i = 0 ; postfix[i] != -1 ; i++)
+			{
+				if(postfix[i] >= 0)	printf("%d ", postfix[i]);
+				else printf("%s ", toChar[ (-postfix[i]) ]);
+			}
+		printf("\n");
+		toValue();
+	}
+	return 0;
+}
---
+++ dsa_3/hw3_2.cpp
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stack>
+#include <iostream>
+
+#define MAX 1100000
+#define toChar_MAX 200
+#define toInt_MAX 256
+#define pow_MAX 7
+#define MIN 12
+#define PLU 13
+#define MUL 20
+#define DIV 21
+#define ADD 30
+#define SUB 31
+#define PAR 100
+#define SIN 102
+#define COS 103
+#define EXP 104
+#define LOG 105
+#define POW 106
+#define SQR 107
+#define FAB 108
+
+/*
+
+Sign-Value Table
+			(m)	(p)
+Sign		-	+	*	/	+	-	(		s		c		e		l		p		r		f
+Value		-12	-13	-20	-21	-30	-31	-100	-102	-103	-104	-105	-106	-107	-108
+
+*/
+
+class Postfix
+{
+public:
+	int c;
+	double v;
+};
+
+char infix[MAX],tmp[MAX], toChar[toChar_MAX][5];
+int toInt[toInt_MAX];
+Postfix postfix[MAX];
+
+void toPostfix()
+{
+	std::stack<int> op_stack;
+	int i = 0, p = 0;
+	while(infix[i] != '\n')
+	{
+		if(infix[i] <= '9' && infix[i] >= '0')
+		{
+			double power[pow_MAX] = {0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001};
+			int index_pow = 0;
+			postfix[p].c = 1;
+			postfix[p].v = (int)(infix[i++] - '0');
+			while(infix[i] <= '9' && infix[i] >= '0')	postfix[p].v = postfix[p].v * 10 + (int)(infix[i++] - '0');
+			if(infix[i] == '.')	i++;
+			while(infix[i] <= '9' && infix[i] >= '0')	postfix[p].v = postfix[p].v + power[index_pow++] * (int)(infix[i++] - '0');
+			p++;	i--;
+		}
+		else if(infix[i] == '(' || infix[i] == 'm')	
+		{
+			op_stack.push( toInt[ (int)infix[i] ] );
+			printf("Push \"%s\" into the stack\n", toChar[ -op_stack.top() ]);
+		}
+		else if(infix[i] == ')')
+		{
+			while(op_stack.top() > toInt[ (int)'(' ])
+			{
+				postfix[p++].c = op_stack.top();
+				printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ]);
+				op_stack.pop();
+			}
+			if(op_stack.top() < toInt[ (int)'(' ])	postfix[p++].c = op_stack.top();
+			printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ]);
+			op_stack.pop();
+		}
+		else if(infix[i] == ',')
+		{
+			while(op_stack.top() > toInt[ (int)'(' ])
+			{
+				postfix[p++].c = op_stack.top();
+				printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ]);
+				op_stack.pop();
+			}
+		}
+		else if(infix[i] == 's' || infix[i] == 'c' || infix[i] == 'e' || infix[i] == 'l' || infix[i] == 'p' || infix[i] == 'f')
+		{
+			int pos = toInt[ (int)infix[i] ];
+			if(infix[i + 2] == 'r')	pos = toInt[ (int)'r'];
+			if(infix[i + 3] != '(')	i++;
+			op_stack.push(pos);
+			printf("Push \"%s\" into the stack\n", toChar[ -op_stack.top() ]);
+			i += 3;
+		}
+		else
+		{
+			while((!op_stack.empty()) && ((-op_stack.top()) / 10) <= ((-toInt[ (int)infix[i] ]) / 10))
+			{
+				postfix[p++].c = op_stack.top();
+				printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ]);
+				op_stack.pop();
+			}
+			op_stack.push( toInt[ (int)infix[i] ] );
+			printf("Push \"%s\" into the stack\n", toChar[ -op_stack.top() ]);
+		}
+		i++;
+	}
+	while(!op_stack.empty())
+	{
+		postfix[p++].c = op_stack.top();
+		printf("Pop \"%s\" from the stack\n", toChar[ -op_stack.top() ]);
+		op_stack.pop();
+	}
+	postfix[p].c = -1;
+	return;
+}
+
+void toValue()
+{
+	std::stack<double> val_stack;
+	int p = 0;
+	double v_1 = 0, v_2 = 0;
+	while(postfix[p].c != -1)
+	{
+		if(postfix[p].c >= 0)	val_stack.push(postfix[p].v);
+		else
+		{
+			v_2 = val_stack.top();
+			val_stack.pop();
+			if(postfix[p].c == -MIN)	val_stack.push(-v_2);
+			else if(postfix[p].c == -SIN)	val_stack.push(sin(v_2));
+			else if(postfix[p].c == -COS)	val_stack.push(cos(v_2));
+			else if(postfix[p].c == -EXP)	val_stack.push(exp(v_2));
+			else if(postfix[p].c == -LOG)	val_stack.push(log(v_2));
+			else if(postfix[p].c == -SQR)	val_stack.push(sqrt(v_2));
+			else if(postfix[p].c == -FAB)	val_stack.push(fabs(v_2));
+			else
+			{
+				v_1 = val_stack.top();
+				val_stack.pop();
+				if(postfix[p].c == -ADD)	val_stack.push(v_1 + v_2);
+				else if(postfix[p].c == -SUB)	val_stack.push(v_1 - v_2);
+				else if(postfix[p].c == -MUL)	val_stack.push(v_1 * v_2);
+				else if(postfix[p].c == -DIV)	val_stack.push(v_1 / v_2);
+				else if(postfix[p].c == -POW)	val_stack.push(pow(v_1, v_2));
+			}
+		}
+		p++;
+	}
+	if(val_stack.size() != 1)	printf("Error Form.\n");
+	else	printf("RESULT: %lf\n", val_stack.top());
+	return;
+}
+
+int main()
+{
+	toInt[(int)'+'] = -ADD;	strcpy(toChar[ADD], "+");
+	toInt[(int)'-'] = -SUB;	strcpy(toChar[SUB], "-");
+	toInt[(int)'*'] = -MUL;	strcpy(toChar[MUL], "*");
+	toInt[(int)'/'] = -DIV;	strcpy(toChar[DIV], "/");
+	toInt[(int)'m'] = -MIN;	strcpy(toChar[MIN], "m");
+	toInt[(int)'('] = -PAR; strcpy(toChar[PAR], "(");
+	toInt[(int)'s'] = -SIN;	strcpy(toChar[SIN], "sin");
+	toInt[(int)'c'] = -COS;	strcpy(toChar[COS], "cos");
+	toInt[(int)'e'] = -EXP;	strcpy(toChar[EXP], "exp");
+	toInt[(int)'l'] = -LOG;	strcpy(toChar[LOG], "log");
+	toInt[(int)'p'] = -POW;	strcpy(toChar[POW], "pow");
+	toInt[(int)'r'] = -SQR;	strcpy(toChar[SQR], "sqrt");
+	toInt[(int)'f'] = -FAB;	strcpy(toChar[FAB], "fabs");
+	while(fgets(tmp, MAX, stdin) != NULL)
+	{
+		if(tmp[1] == '\0')	break;
+		int j = 0;
+		for(int i = 0 ; tmp[i] != '\n' ; i++)
+			if(tmp[i] != ' ')
+			{
+				if(tmp[i] == '+' || tmp[i] == '-')
+					if(j == 0 || ((infix[j - 1] > '9' || infix[j - 1] < '0') && infix[j - 1] != ')'))
+					{
+						if(tmp[i] == '-')	infix[j++] = 'm';
+						continue;
+					}
+				infix[j++] = tmp[i];
+			}
+		infix[j] = '\n';
+		toPostfix();
+		printf("Postfix Expression: ");
+			for(int i = 0 ; postfix[i].c != -1 ; i++)
+			{
+				if(postfix[i].c >= 0)	printf("%lf ", postfix[i].v);
+				else printf("%s ", toChar[ (-postfix[i].c) ]);
+			}
+		printf("\n");
+		toValue();
+	}
+	return 0;
+}
